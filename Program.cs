300b7cba6905036e09b5061dde04eaba385cc482
KeirMcGlinn-Shaw <k.mcglinn-shaw@newcastle.ac.uk>
2020-06-24 20:29:11 +0100

    Added code required to bring the bot 'online'

--- Program.cs
+++ Program.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
 
 namespace AtmosMusicBot
 {
     class Program
     {
-        static void Main(string[] args)
+        /*
+         *  Main method synchronus
+         */
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        // Used to connect to Discord
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            // Need to move this and store it securely
+            var token = "NzI1NDA3ODY5OTA5OTkxNDU1.XvOmVw.A2uW0BEHdz22FpmbbHOO3wVwYgM";
+
+            await _client.LoginAsync(TokenType.Bot, token);
+
+            await _client.StartAsync();
+
+            // Block this task until the program is closed
+            await Task.Delay(-1);
         }
+
+        /* 
+         * Logging method
+         * 
+         * Logs to Console
+         */
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+
     }
 }
